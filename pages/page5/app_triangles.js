5b849dd449eefe3db4b5f4a5deacbb766a64cc8e
Nil <xetaxe@gmail.com>
2023-03-01 01:09:22 +0100

    Add gravity logic (triangles attracted to mouse)

--- pages/page5/app_triangles.js
+++ pages/page5/app_triangles.js
@@ -2,6 +2,8 @@ import {createShader, createProgram, randomInt} from "./utils.js";
 import { Triangle } from "./triangle.js";
 
 let canvas = document.getElementById("myCanvas");
+let mouse = {x: 0, y: 0}
+let MASS = 0.001;
 
 // Set canvas dimensions
 canvas.width = innerWidth;
@@ -17,11 +19,22 @@ if(!gl) {
 if(!gl)
     alert("Your browser does not support WebGL");
 
+window.onresize = () => {
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+	webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+}
+
+window.onmousemove = (e) => {
+	mouse.x = e.x;
+	mouse.y = e.y;
+}
 
 //Create the two shaders and link them into a program
 let vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
 let fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
- 
+
 let vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
 let program = createProgram(gl, vertexShader, fragmentShader);
@@ -44,20 +57,6 @@ let stride = 0;        // 0 = move forward size * sizeof(type) each iteration to
 let offset = 0;        // start at the beginning of the buffer
 let primitiveType = gl.TRIANGLES; // 2d, we set triangles as main n-simplex
 
-
-////// Up until that point we were dealing with INITIALIZATION CODE
-////// It gets run once when we load the page.
-////// The code below is RENDERING CODE
-////// It should get executed each time we want to render/draw
-
-//Resize the canvas
-webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-// Clear the canvas
-gl.clearColor(0, 0, 0, 0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
 //Tell WebGL the program to execute and set resolution (2d)
 gl.useProgram(program);
 gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
@@ -68,35 +67,80 @@ gl.enableVertexAttribArray(positionAttributeLocation);
 // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
 gl.vertexAttribPointer( positionAttributeLocation, size, type, normalize, stride, offset);
 
-//Tell WebGL to execute our GLSL program
-
 
 // draw 50 random triangles in random colors
 let triangles = []
 
 for (var i = 0; i < 50; i++) {
   let randomColor = [Math.random(), Math.random(), Math.random(), 1];
-  let triangle = new Triangle(randomInt(300, 0), randomInt(300, 0), randomInt(50, 10), randomInt(50, 10), randomColor, randomInt(50, 10), randomInt(50, 10));
+  let triangle = new Triangle(randomInt(400, 0), randomInt(300, 0), randomInt(50, 10), randomInt(50, 10), randomColor, Math.random()*10, Math.random()*10);
 	triangles.push(triangle)
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle.getPoints()), gl.STATIC_DRAW);
-  gl.uniform4f(colorUniformLocation, ...triangle.getColor());
-
-  // Draw the triangles (first render).
-  gl.drawArrays(primitiveType, 0, triangles.length);
 }
+
+
+let lastLoop = new Date();
 
 function render(now) {
 
+	//Measures FPS of the animation
+	let thisLoop = new Date();
+	let fps = 1000 / (thisLoop - lastLoop);
+	lastLoop = thisLoop;
+	console.log("fps:" + fps);
+
+	// Clear the canvas
+	// gl.clearColor(0, 0, 0, 0);
+	// gl.clear(gl.COLOR_BUFFER_BIT);
+
+	//Tell WebGL the program to execute and set resolution (2d)
+	gl.useProgram(program);
+	gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+	//We turn attribute on
+	// gl.enableVertexAttribArray(positionAttributeLocation);
+
+	// Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+	// gl.vertexAttribPointer( positionAttributeLocation, size, type, normalize, stride, offset);
+
+	// handleCollisions(triangles)
+;
 	triangles = triangles.map(triangle => {
+		handleGravity(triangle, mouse);
+		handleCollisions(triangle);
 		triangle.updatePoints();
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle.getPoints()), gl.STATIC_DRAW);
 		gl.uniform4f(colorUniformLocation, ...triangle.getColor());
-	
+
 		// Draw the triangles (first render).
 		gl.drawArrays(primitiveType, 0, triangles.length);
+
+		return triangle
 	})
 
 	requestAnimationFrame(render);
 }
 
 render();
+
+function handleCollisions(triangle){
+
+	//Make triangles bounce if limit of canvas is reached
+	if (triangle.x + triangle.vx > canvas.width)
+		triangle.vx = -triangle.vx
+	else if (triangle.x < 0)
+		triangle.vx = -triangle.vx
+	if (triangle.y + triangle.height > canvas.height)
+		triangle.vy = -triangle.vy
+	else if (triangle.y < 0)
+		triangle.vy = -triangle.vy
+
+}
+
+function handleGravity(triangle, mouse){
+	let dx = (mouse.x - triangle.x);
+	let dy = (mouse.y - triangle.y);
+	let d = Math.sqrt(dx*dx + dy*dy);
+	console.log(mouse.x, mouse.y, d);
+	triangle.vx += MASS * (dx / d*d);
+	triangle.vy += MASS * (dy / d*d);
+}
